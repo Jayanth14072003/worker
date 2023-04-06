ce86ddf865bf0a98d8cd81584ee39dfe73aec94f
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-06 21:53:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -58,7 +58,7 @@ async def start_command(client: Client, message: Message):
         temp_msg = await message.reply("Please wait...")
         try:
             messages = await get_messages(client, ids)
-            await asyncio.sleep(10)
+            messages.delete(10)
         except:
             await message.reply_text("Something went wrong..!")
             return
