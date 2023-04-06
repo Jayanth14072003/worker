325537f540f428ad74e0d6e51ac4e7af316fc91a
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-07 01:11:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 
 
-import os getenv
+import os environ
 import os
 import logging
 from logging.handlers import RotatingFileHandler
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001693231644"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "1061576483"))
 
 #shortenr
-API = str(getenv('API','eedc409c6457b8c783019e990dde8fd531b58eca'))
+API = environ.get('API','eedc409c6457b8c783019e990dde8fd531b58eca'))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
