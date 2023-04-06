8dd81210c155883ac78eac154003c2d756ce68fa
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-06 23:19:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -81,18 +81,19 @@ async def start_command(client: Client, message: Message):
                 na = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
                 naa = await na.reply_text(f"<b>Here is your link</b>", quote=True, reply_markup=reply_markup)
-                await na.delete()
-                await asyncio.sleep(50)
-                await naa.delete(50)
+                await asyncio.sleep(10)
+                await naa.delete()
                 
+                await na.delete()
                 
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 na = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await na.reply_text(f"<b>Here is your link</b>", quote=True, reply_markup=reply_markup)
+                await asyncio.sleep(10)
+                await naa.delete()
+                
                 await na.delete()
-                await asyncio.sleep(50)
-                await naa.delete(50)
             except:
                 pass
         return
