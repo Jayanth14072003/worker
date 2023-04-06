5eb91a84191d5cef779fd9d9610b2bec7b12368e
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-06 23:22:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -80,8 +80,8 @@ async def start_command(client: Client, message: Message):
             try:
                 na = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
-                naa = await na.reply_text(f"<b>Here is your link</b>", quote=True, reply_markup=reply_markup)
-                await asyncio.sleep(10)
+                naa = await na.reply_text(f"<b>ಈ ಫೈಲ್ ಒಂದು ಗಂಟೆಯ ನಂತರ ಡಿಲೀಟ್ ಆಗುತ್ತದೆ, ಆದ್ದರಿಂದ ಈ ಫೈಲ್ ಅನ್ನು ಫಾರ್ವರ್ಡ್ ಅಥವಾ ಸೇವ್ ಮಾಡಿಕೊಳ್ಳಿ</b>", quote=True, reply_markup=reply_markup)
+                await asyncio.sleep(3600)
                 await naa.delete()
                 
                 await na.delete()
@@ -89,8 +89,8 @@ async def start_command(client: Client, message: Message):
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 na = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                await na.reply_text(f"<b>Here is your link</b>", quote=True, reply_markup=reply_markup)
-                await asyncio.sleep(10)
+                await na.reply_text(f"<b>ಈ ಫೈಲ್ ಒಂದು ಗಂಟೆಯ ನಂತರ ಡಿಲೀಟ್ ಆಗುತ್ತದೆ, ಆದ್ದರಿಂದ ಈ ಫೈಲ್ ಅನ್ನು ಫಾರ್ವರ್ಡ್ ಅಥವಾ ಸೇವ್ ಮಾಡಿಕೊಳ್ಳಿ</b>", quote=True, reply_markup=reply_markup)
+                await asyncio.sleep(360)
                 await naa.delete()
                 
                 await na.delete()
