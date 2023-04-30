1ca1d50c76a6563c8ccf96c269a3e7195948888b
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-30 21:05:06 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -4,9 +4,9 @@ import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
-
+from plugins.link_generator import get_short
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON, API
+from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
@@ -24,10 +24,12 @@ async def channel_post(client: Client, message: Message):
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    link = https://telegram.me/{client.username}?start={base64_string}
+    slink = get_short(f"https://telegram.me/{client.username}?start={base64_string}") #we can use "linl" insted of "f"https://telegram.me/{client.username}?start={base64_string}""
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=link)]])
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}\nTG link\n<code>{link}</code>\nShort Link\n<code>{slink}</code>", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Pri᥎ᥲᴛᥱ ᥣiᥒκ 🔗</b>\n<code>https://telegram.me/{client.username}?start={base64_string}</code> \n\n<b>TG ᥣiᥒκ😎</b>\n<code>{link}</code>\nShort link\n<code>{slink}</code> ", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
@@ -41,8 +43,10 @@ async def new_post(client: Client, message: Message):
     converted_id = message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
+    link = https://telegram.me/{client.username}?start={base64_string}
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+  
+    
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
